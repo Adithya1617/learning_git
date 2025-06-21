3c0084359c33875decaa8fee3f837ebc489af6c0
Adithya <adithya.vardhan1617@gmail.com>
2025-06-21 13:02:32 +0530

    added a profile page endpoint

--- learn git/app.py
+++ learn git/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/profile')
+def profile():
+    return 'This is the profile page.'
+
 if __name__ == '__main__':
     app.run(debug=True) 
